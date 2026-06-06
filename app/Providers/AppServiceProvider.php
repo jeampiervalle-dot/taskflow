dff38448bc13d8b069bc41fc48401d07d21617ab
jean <jeampiervalle@gmail.com>
2026-06-06 00:20:36 -0500

    Refactor: usar trustProxies para detectar HTTPS tras proxy inverso

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (request()->isSecure() || request()->header('X-Forwarded-Proto') === 'https') {
+        if (request()->isSecure()) {
             URL::forceScheme('https');
         }
     }
