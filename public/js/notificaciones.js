3a574dc1bcf692596749e3b73970310fac9d1ac5
jean <jeampiervalle@gmail.com>
2026-06-11 14:04:27 -0500

    Cleanup: remove unused CSS, JS, and image files

--- public/js/notificaciones.js
+++
@@ -1,9 +0,0 @@
-function closeToast(id) {
-    const t = document.getElementById(id);
-    if (t) t.classList.add('hidden');
-}
-
-setTimeout(() => {
-    const t = document.getElementById('systemToast');
-    if (t) t.classList.add('hidden');
-}, 4000);
